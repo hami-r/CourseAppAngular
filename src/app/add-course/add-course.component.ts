8a057bfb4e91765ce4ff826a9638967bf361d1b5
hami-r <mynameissaitama54@gmail.com>
2022-11-30 09:30:58 +0530

    added data to api

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -13,15 +14,24 @@ export class AddCourseComponent {
   courseDate = "";
   courseVenue = "";
 
+  constructor(private api:ApiService) {}
+
   readValues = () => {
-    let data = {
+    let data:any = {
    "courseTitle":this.courseTitle,
    "courseDescription":this.courseDescription,
    "courseDuration":this.courseDuration,
    "courseDate":this.courseDate,
    "courseVenue":this.courseVenue };
-
    console.log(data);
-  }
    
+   this.api.addCourse(data).subscribe(
+    (response) => {
+      console.log(response)
+    }
+   )
+
+  }
+
+  
 }
